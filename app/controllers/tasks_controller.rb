68c289bc6fbea509e298327a070ca39f5ebb61f1
ljones1877 <jlorin7@gmail.com>
2019-12-24 17:28:10 +0000

    L15 3 tests 0 errors

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,15 +3,20 @@ class TasksController < ApplicationController
     render json: Task.order(:id)
   end
 
-   def update
+  def update
     task = Task.find(params[:id])
     task.update_attributes(task_params)
+    render json: task
+  end
+
+  def create
+    task = Task.create(task_params)
     render json: task
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:done)
+    params.require(:task).permit(:done, :title)
   end
 end
